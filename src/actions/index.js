a8a62f0067c0d156c0ba180f3aeb5938ab1cf3f0
sekosolares <axel.solares.g@gmail.com>
2022-07-13 13:53:33 -0600

    Update: refactor fetchUser dispatching with lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,8 +16,13 @@ export const fetchUser = id => async dispatch => {
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // _.chain() basically executes from .value() to the top.
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const addPost = () => {
