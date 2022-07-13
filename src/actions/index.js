3d21f20d73971698fb49eeeacb3cc174f1abc61b
sekosolares <axel.solares.g@gmail.com>
2022-07-13 12:28:01 -0600

    Update: Finish fetchPostsAndUsers action creator and remove logic from UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async (dispatch) => {
@@ -7,13 +7,6 @@ export const fetchPosts = () => async (dispatch) => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-// const _fetchUser = _.memoize(async (id, dispatch) => {
-//     const response = await jsonPlaceholder.get(`/users/${id}`)
-
-//     dispatch({ type: 'FETCH_USER', payload: response.data })
-// });
-// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
 
 export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`)
@@ -21,10 +14,10 @@ export const fetchUser = id => async dispatch => {
     dispatch({ type: 'FETCH_USER', payload: response.data })
 };
 
-export const fetchPostsAndUsers = () => async dispatch => {
-    console.log('About to fetch posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    console.log('fetched posts');
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 export const addPost = () => {
